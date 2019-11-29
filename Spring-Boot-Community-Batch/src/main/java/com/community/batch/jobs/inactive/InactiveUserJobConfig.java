a66602bd83fcf0997a80ce40be7aa9512161a639
frankyoh <frankyoh@tmon.co.kr>
2019-11-29 18:15:25 +0900

    소스코드 추가

---
+++ Spring-Boot-Community-Batch/src/main/java/com/community/batch/jobs/inactive/InactiveUserJobConfig.java
@@ -0,0 +1,111 @@
+package com.community.batch.jobs.inactive;
+
+import com.community.batch.domain.User;
+import com.community.batch.domain.enums.UserStatus;
+import com.community.batch.jobs.inactive.listener.InactiveChunkListener;
+import com.community.batch.jobs.inactive.listener.InactiveIJobListener;
+import com.community.batch.jobs.inactive.listener.InativeStepListener;
+import com.community.batch.repository.UserRepository;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * Created by KimYJ on 2018-03-07
+ */
+@Configuration
+@AllArgsConstructor
+public class InactiveUserJobConfig {
+    private final static int CHUNK_SIZE = 5;
+
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Bean
+    public Job inactiveUserJob(JobBuilderFactory jobBuilderFactory, InactiveIJobListener inactiveIJobListener, Step inactiveJobStep) {
+        return jobBuilderFactory.get("inactiveUserJob")
+                .preventRestart()
+                .listener(inactiveIJobListener)
+                .start(inactiveJobStep)
+                .build();
+    }
+
+    @Bean
+    public Step inactiveJobStep(StepBuilderFactory stepBuilderFactory, ListItemReader<User> inactiveUserReader, InactiveChunkListener inactiveChunkListener, InativeStepListener inativeStepListener, TaskExecutor taskExecutor) {
+        return stepBuilderFactory.get("inactiveUserStep")
+                .<User, User> chunk(CHUNK_SIZE)
+                .reader(inactiveUserReader)
+                .processor(inactiveUserProcessor())
+                .writer(inactiveUserWriter())
+                .listener(inactiveChunkListener)
+                .listener(inativeStepListener)
+                .taskExecutor(taskExecutor)
+                .throttleLimit(2)
+                .build();
+    }
+
+    /*@Bean
+    public Step inactiveJobStep(StepBuilderFactory stepBuilderFactory, InactiveItemTasklet inactiveItemTasklet) {
+        return stepBuilderFactory.get("inactiveUserTaskleyStep")
+                .tasklet(inactiveItemTasklet)
+                .build();
+    }*/
+
+    @Bean
+    public TaskExecutor taskExecutor(){
+        return new SimpleAsyncTaskExecutor("Batch_Task");
+    }
+
+    @Bean
+    @StepScope
+    public ListItemReader<User> inactiveUserReader(@Value("#{jobParameters[nowDate]}") Date nowDate, UserRepository userRepository) {
+        LocalDateTime now = LocalDateTime.ofInstant(nowDate.toInstant(), ZoneId.systemDefault());
+        List<User> inactiveUsers = userRepository.findByUpdatedDateBeforeAndStatusEquals(now.minusYears(1), UserStatus.ACTIVE);
+        return new ListItemReader<>(inactiveUsers);
+    }
+
+    /*@Bean(destroyMethod="")
+    @StepScope
+    public JpaPagingItemReader<User> inactiveUserJpaReader(@Value("#{jobParameters[nowDate]}") Date nowDate) {
+        JpaPagingItemReader<User> jpaPagingItemReader = new JpaPagingItemReader<>();
+        jpaPagingItemReader.setQueryString("select u from User as u where u.createdDate < :createdDate and u.status = :status");
+
+        Map<String, Object> map = new HashMap<>();
+        LocalDateTime now = LocalDateTime.ofInstant(nowDate.toInstant(), ZoneId.systemDefault());
+        map.put("createdDate", now.minusYears(1));
+        map.put("status", UserStatus.ACTIVE);
+
+        jpaPagingItemReader.setParameterValues(map);
+        jpaPagingItemReader.setEntityManagerFactory(entityManagerFactory);
+        jpaPagingItemReader.setPageSize(CHUNK_SIZE);
+        return jpaPagingItemReader;
+    }*/
+
+    private InactiveItemProcessor inactiveUserProcessor() {
+        return new InactiveItemProcessor();
+    }
+
+    private JpaItemWriter<User> inactiveUserWriter() {
+        JpaItemWriter<User> jpaItemWriter = new JpaItemWriter<>();
+        jpaItemWriter.setEntityManagerFactory(entityManagerFactory);
+        return jpaItemWriter;
+    }
+}
